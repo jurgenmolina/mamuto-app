003cba8154ed89bf2ae05b8a0a2e55c137cb5fa5
jurgenmolina <jurgen.molina@cetus.com.co>
2024-06-29 17:14:10 -0500

    Desarrollo nav bar

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [NgbCarouselModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  slides = [
+    {
+      id: 1,
+      src: './assets/images/home1.jpg',
+      title: 'Another Title',
+      subtitle: 'Another subtitle...'
+    },
+    {
+      id: 2,
+      src: './assets/images/home2.jpg',
+      title: 'Another Title',
+      subtitle: 'Another subtitle...'
+    },
+    {
+      id: 3,
+      src: './assets/images/home3.jpg',
+      title: 'Another Title',
+      subtitle: 'Another subtitle...'
+    }
+  ];
+}
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -2,20 +2,15 @@ import { Component, HostListener } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { NgIf } from '@angular/common';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, NgIf],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, NgClass],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  isScrolled = false;
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    this.isScrolled = window.pageYOffset > 50;
-  }
+  
 }
